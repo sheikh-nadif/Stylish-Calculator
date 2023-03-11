ea7ab69632aad6b700dd324a00bb021321644a3b
sheikh-nadif <93081748+sheikh-nadif@users.noreply.github.com>
2023-03-11 16:51:02 +0600

    done with dark theme

--- script.js
+++ script.js
@@ -2,26 +2,30 @@ let buttons = document.querySelector(".buttons");
 let btn = document.querySelectorAll("span");
 let value = document.getElementById("value");
 let counter = document.getElementById("count");
-let count = 12;
+let count = 14;
 
 for(let i=0; i<btn.length; i++){
     btn[i].addEventListener("click", function(){ 
-        //if value.length .......
+        
         if(this.innerHTML == "="){
             const result = eval(value.innerHTML);
             const strResult = result.toString()
             value.innerHTML = strResult.substring(0,14);
-            count = 12;
-            counter.innerHTML = "12";
+            count = 14;
+            counter.innerHTML = "14";
         }
         else{
             if(this.innerHTML == "Clear"){
                 value.innerHTML = "";
-                count = 12;
-                counter.innerHTML = "12";
+                count = 14;
+                counter.innerHTML = "14";
             }
             else{
-                    value.innerHTML += this.innerHTML;
+                    if(value.innerHTML.length > 13){
+                        value.innerHTML += " ";
+                    }
+                    else {
+                        value.innerHTML += this.innerHTML;
 
                     count -- ;
                     if (count >= 10){
@@ -33,7 +37,23 @@ for(let i=0; i<btn.length; i++){
                     else{
                         counter.innerHTML = "Full";
                     }
+                    }
                 }
-        } //If.........
+        } 
     })
+}
+
+
+
+let themeBTN = document.getElementById("theme");
+let body = document.querySelector("body");
+let iframe = document.querySelector("i");
+let theme = iframe.classList.contains('fa-moon-o');
+themeBTN.onclick = function (){
+    body.classList.toggle('dark-body');
+    iframe.classList.toggle('fa-sun-o');
+    iframe.classList.toggle('fa-moon-o');
+    value.innerHTML = "";
+    count = 14;
+    counter.innerHTML = "14";
 }
